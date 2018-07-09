a8b670cc13d42fa7868753f78adb3465a8da98df
junseok95 <37564074+junseok95@users.noreply.github.com>
2018-07-09 15:06:57 +0900

    Add files via upload

---
+++ Button.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    private void OnTriggerStay(Collider other)
+    {
+        StartCoroutine(getdown());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        StartCoroutine(getup());
+    }
+
+    IEnumerator getdown()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime/50);
+            yield return new WaitForSeconds(0.01f);
+        }  
+    }
+
+    IEnumerator getup()
+    {
+        
+        while(true)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime/10);
+            yield return new WaitForSeconds(0.01f);
+            if (this.gameObject.transform.position.y >= 0.0f)
+                break;
+        }
+    }
+}
---
+++ Wind.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour {
+
+    public float windspeed = 5.0f;
+
+
+    [SerializeField]
+    private GameObject character;
+
+    [SerializeField]
+    private GameObject des;
+
+    private Rigidbody rb;
+    private bool winding = false;
+
+    private Vector3 temp;
+
+    private void Start()
+    {
+        rb = character.GetComponent<Rigidbody>();
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    character.GetComponent<Rigidbody>().useGravity = false;
+    //}
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag =="Player")
+        {
+       
+          //  Debug.Log("strcor");
+            if (!winding)
+            {
+
+                winding = true;
+                StartCoroutine(getback(other));
+
+            }
+ 
+        }
+    }
+
+    //temp = transform.rotation* Vector3.back;
+
+    IEnumerator getback(Collider other)
+    {
+        while (winding)
+        {
+           Debug.Log("AddForce");
+            // other.GetComponent<Rigidbody>().AddForce(transform.rotation * Vector3.back * windspeed);
+
+            temp = new Vector3(des.transform.position.x - character.transform.position.x, des.transform.position.y - character.transform.position.y, des.transform.position.z - character.transform.position.z);
+ 
+
+            // other.transform.Translate(temp.normalized * windspeed);
+
+            //other.transform.position.Set(other.transform.position.x - temp.normalized.x *10, other.transform.position.y - temp.normalized.y * 10, other.transform.position.z - temp.normalized.z * 10);
+
+            other.transform.position += temp.normalized * windspeed;
+
+
+            // other.transform.Translate((c.transform.rotation * Vector3.back * windspeed));
+            //yield return null;
+            //yield return new WaitForSeconds(0.03f);
+            //yield return new WaitForEndOfFrame();
+
+            yield return new WaitForSeconds(0.011f);
+
+
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("strcor");
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            other.GetComponent<Rigidbody>().isKinematic = false;
+            winding = false;
+            //character.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
